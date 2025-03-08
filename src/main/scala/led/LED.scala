28976b727dbb0388fd764a9b2797eb9f5f93595e
wangfiox <wangfiox@gmail.com>
2025-03-08 23:27:50 +0800

    traffic_light

--- src/main/scala/led/LED.scala
+++ src/main/scala/led/LED.scala
@@ -5,82 +5,57 @@ package led
 import chisel3._
 import chisel3.util._
 
-// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
-import _root_.circt.stage.ChiselStage
+object TrafficLightEnum extends ChiselEnum {
+  val ns_red_we_green, ns_red_we_yellow, ns_green_we_red, ns_yellow_we_red =
+    Value
+}
 
-// sw == 1 && k == 1 -> 两边向中间
-// sw == 0 && k == 1 -> 中间向两边
-// sw == 1 && k == 0 -> 循环左移
-// sw == 0 && k == 0 -> 循环右移
-class TopModule extends Module {
+
+class TrafficLight extends Module {
   val io = IO(new Bundle {
-    val sw = Input(Bool())
-    val k = Input(Bool())
-    val led = Output(UInt(8.W))
+    val ns_red = Output(Bool())
+    val ns_yellow = Output(Bool())
+    val ns_green = Output(Bool())
+    val we_red = Output(Bool())
+    val we_yellow = Output(Bool())
+    val we_green = Output(Bool())
   })
 
-  def shiftLeftRotate(value: UInt, width: Int): UInt = {
-    val head = value(width - 1)
-    val tail = value(width - 2, 0)
-    tail ## head
-  }
+  val cnt = Counter(100_000_000) // 100M * 1s
 
-  def shiftRightRotate(value: UInt, width: Int): UInt = {
-    val init = value(width - 1, 1)
-    val last = value(0)
-    last ## init
-  }
+  // 用于指示持续的时间
+  val last = Counter(3)
 
-  val led = RegInit(0.U(8.W))
-  val cnt = Counter(50_000_000) // 100M * 0.5 = 50M
+  val state = Reg(TrafficLightEnum())
 
-  val last = RegInit(0.U(2.W))
+  // 状态切换
   when(cnt.inc()) {
-    last := io.sw ## io.k
-  }
-
-  val fire = last =/= (io.sw ## io.k)
-
-  when(cnt.inc()) {
-    when(led === 0.U || fire) {
-      when(io.k === 1.U && io.sw === 1.U) {
-        led := "b1000_0001".U
+    switch(state) {
+      is(TrafficLightEnum.ns_red_we_green) {
+        when(last.inc()) {
+          state := TrafficLightEnum.ns_red_we_yellow
+        }
       }
-      when(io.k === 1.U && io.sw === 0.U) {
-        led := "b0001_1000".U
+      is(TrafficLightEnum.ns_red_we_yellow) {
+        state := TrafficLightEnum.ns_green_we_red
       }
-      when(io.k === 0.U && io.sw === 1.U) {
-        led := "b0000_0001".U
+      is(TrafficLightEnum.ns_green_we_red) {
+        when(last.inc()) {
+          state := TrafficLightEnum.ns_yellow_we_red
+        }
       }
-      when(io.k === 0.U && io.sw === 0.U) {
-        led := "b1000_0000".U
-      }
-    }.otherwise {
-      val high = led(7, 4)
-      val low = led(3, 0)
-      when(io.k === 1.U && io.sw === 1.U) {
-        led := shiftRightRotate(high, 4) ## shiftLeftRotate(low, 4)
-      }
-      when(io.k === 1.U && io.sw === 0.U) {
-        led := shiftLeftRotate(high, 4) ## shiftRightRotate(low, 4)
-      }
-      when(io.k === 0.U && io.sw === 1.U) {
-        led := shiftLeftRotate(led, 8)
-      }
-      when(io.k === 0.U && io.sw === 0.U) {
-        led := shiftRightRotate(led, 8)
+      is(TrafficLightEnum.ns_yellow_we_red) {
+        state := TrafficLightEnum.ns_red_we_green
       }
     }
   }
 
-  io.led := led
-}
+  // 状态输出
+  io.ns_red := state === TrafficLightEnum.ns_red_we_green || state === TrafficLightEnum.ns_red_we_yellow
+  io.ns_yellow := state === TrafficLightEnum.ns_yellow_we_red
+  io.ns_green := state === TrafficLightEnum.ns_green_we_red
+  io.we_red := state === TrafficLightEnum.ns_green_we_red || state === TrafficLightEnum.ns_yellow_we_red
+  io.we_yellow := state === TrafficLightEnum.ns_red_we_yellow
+  io.we_green := state === TrafficLightEnum.ns_red_we_green
 
-/** Generate Verilog sources and save it in file GCD.v
-  */
-object TopModule extends App {
-  ChiselStage.emitSystemVerilogFile(
-    new TopModule,
-    firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
-  )
 }
---
+++ src/main/scala/led/TopModule.scala
@@ -0,0 +1,32 @@
+package led
+
+import chisel3._
+
+class TopModule extends RawModule {
+  val io = IO(new Bundle {
+    val pll_inst1_CLKOUT0 = Input(Clock())
+    val pll_inst1_LOCKED = Input(Bool())
+    val led_data = Output(UInt(8.W))
+  })
+
+  val led =
+    withClockAndReset(io.pll_inst1_CLKOUT0, !io.pll_inst1_LOCKED) {
+      Module(new TrafficLight)
+    }
+
+  io.led_data := false.B ## false.B ## !led.io.ns_red ## !led.io.ns_yellow ## !led.io.ns_green ## !led.io.we_red ## !led.io.we_yellow ## !led.io.we_green
+
+}
+
+// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
+import _root_.circt.stage.ChiselStage
+
+/** Generate Verilog sources and save it in file GCD.v
+  */
+object TopModule extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new TopModule,
+    Array("--target-dir", "generated"),
+    firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
+  )
+}
